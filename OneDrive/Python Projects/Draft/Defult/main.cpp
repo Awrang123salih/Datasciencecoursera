ee574e8d68542c8843be9bc14e060e7c5875a0f7
Awrang123salih <awrangsalihee@outlook.com>
2022-03-25 13:10:37 -0400

    initial commit

---
+++ OneDrive/Python Projects/Draft/Defult/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    string charactername = "John";
+    int characterage = "45";
+    cout characterpersonality = "he is a nice kid";
+
+
+
+
+
+
+    return 0;
+}
---
+++ OneDrive/Python Projects/my first app/my first app/my first app.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    printf("wellcome to trinidad and togo");
+    cout << "Hello World!\n";//this will output to the console hello world
+
+    cout << "my name is Awrang.";
+    cout << "today is Friday." <<endl;
+    cout << "the system should rund directrly under the comond of new transistors" <<endl;
+    cout << "have  a nice day." << endl;
+
+
+    system("pause>0"); 
+}
